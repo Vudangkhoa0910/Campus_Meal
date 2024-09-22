244987c87c0d2ad52094fde313462389178db7d2
VuDangKhoa <dangkhoa09102004@gmail.com>
2024-09-22 17:39:07 +0700

    Update home_screen v2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:campus_catalogue/screens/cart.dart';
+import 'package:campus_catalogue/screens/search_screen.dart';
+import 'package:campus_catalogue/screens/shop_info.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:campus_catalogue/constants/colors.dart';
@@ -266,3 +269,201 @@ class LocationCard extends StatelessWidget {
   }
 }
 
+class SearchInput extends StatefulWidget {
+  const SearchInput({super.key});
+  @override
+  State<SearchInput> createState() => _SearchInputState();
+}
+
+class _SearchInputState extends State<SearchInput> {
+  final searchController = TextEditingController();
+  List<String> searchTerms = [];
+  List shopSearchResult = [];
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    searchController.dispose();
+    super.dispose();
+  }
+
+  getSearchResult(searchTerm) async {
+    final searchResult = await FirebaseFirestore.instance
+        .collection("cache")
+        .doc(searchTerm)
+        .get();
+    return searchResult;
+  }
+
+  void searchSubmit(context) async {
+    searchTerms = searchController.text.split(' ');
+    Set shops = {};
+    for (int i = 0; i < searchTerms.length; i++) {
+      var tmp = (await getSearchResult(searchTerms[i]));
+      shopSearchResult = tmp['list'];
+
+      for (var shop in shopSearchResult) {
+        final tmp =
+            await FirebaseFirestore.instance.collection("shop").doc(shop).get();
+        shops.add(tmp.data());
+      }
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => SearchScreen(
+                    shopResults: shops.toList(),
+                    isSearch: true,
+                    title: "Explore IITG",
+                  )));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 15, 25, 0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: TextField(
+                  controller: searchController,
+                  onSubmitted: (e) => searchSubmit(context),
+                  autofocus: false,
+                  cursorColor: Colors.grey,
+                  decoration: InputDecoration(
+                      isDense: true,
+                      fillColor: Colors.white,
+                      filled: true,
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                              width: 1, color: AppColors.backgroundOrange)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                              width: 1, color: AppColors.backgroundOrange)),
+                      hintText: 'Search',
+                      hintStyle:
+                          const TextStyle(color: Colors.grey, fontSize: 18),
+                      suffixIcon: const Icon(
+                        Icons.search,
+                        color: AppColors.secondary,
+                        size: 30,
+                      )),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  Future<List> getCampusFavouriteShops() async {
+    List tmp = [];
+    final shops = await FirebaseFirestore.instance
+        .collection("shop")
+        .orderBy("rating", descending: true)
+        .limit(10)
+        .get();
+    for (var doc in shops.docs) {
+      tmp.add(doc);
+    }
+    return tmp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: const Icon(
+              Icons.arrow_back_ios_new_rounded,
+              color: AppColors.backgroundOrange,
+            ),
+          ),
+          backgroundColor: AppColors.backgroundYellow,
+          elevation: 0,
+          centerTitle: true,
+          title: Text("Explore IITG",
+              style: AppTypography.textMd.copyWith(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  color: AppColors.backgroundOrange)),
+        ),
+        backgroundColor: AppColors.backgroundYellow,
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SearchInput(),
+                  const LocationCardWrapper(),
+                  const ShopHeader(name: "Campus Favourites"),
+                  FutureBuilder<List<dynamic>>(
+                      future: getCampusFavouriteShops(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<List<dynamic>> snapshot) {
+                        if (snapshot.hasData) {
+                          final campusFavs = snapshot.data!;
+                          return ShopCardWrapper(shops: campusFavs);
+                        } else {
+                          return const CircularProgressIndicator(
+                              color: AppColors.backgroundOrange);
+                        }
+                      }),
+                  const ShopHeader(name: "Recommended"),
+                  FutureBuilder<List<dynamic>>(
+                      future: getCampusFavouriteShops(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<List<dynamic>> snapshot) {
+                        if (snapshot.hasData) {
+                          final campusFavs = snapshot.data!;
+                          return ShopCardWrapper(shops: campusFavs);
+                        } else {
+                          return const CircularProgressIndicator(
+                              color: AppColors.backgroundOrange);
+                        }
+                      }),
+                  // ShopCardWrapper(shops: campusFavouriteShops),
+                ],
+              ),
+            ),
+            Cart(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: AppColors.backgroundOrange,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ));
+  }
+}

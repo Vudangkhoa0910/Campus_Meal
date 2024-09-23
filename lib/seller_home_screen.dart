582fd9813a9628926f94b547e519549e6dd6c77f
tuanphan1806 <22010282@st.phenikaa-uni.edu.vn>
2024-09-23 13:38:25 +0700

    Update screens/seller_home v4

--- lib/seller_home_screen.dart
+++ lib/seller_home_screen.dart
@@ -1,7 +1,10 @@
+import 'package:campus_catalogue/add_item.dart';
 import 'package:campus_catalogue/constants/colors.dart';
 import 'package:campus_catalogue/constants/typography.dart';
+import 'package:campus_catalogue/models/order_model.dart';
 import 'package:campus_catalogue/models/shopModel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:campus_catalogue/services/database_service.dart';
 import 'package:flutter/material.dart';
 
 class OrderWrapper extends StatelessWidget {
@@ -209,19 +212,6 @@ class _SellerHomeScreenState extends State<SellerHomeScreen> {
               "Current Orders",
               style: AppTypography.textMd.copyWith(fontWeight: FontWeight.w700),
             ),
-            FutureBuilder<List<dynamic>>(
-                future: getOrders(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<dynamic>> snapshot) {
-                  if (snapshot.hasData) {
-                    final orders = snapshot.data!;
-                    return OrderWrapper(orders: orders);
-                  } else {
-                    return Text("No orders",
-                        style: AppTypography.textMd.copyWith(
-                            fontSize: 20, fontWeight: FontWeight.w700));
-                  }
-                }),
             // SizedBox(
             //   height: MediaQuery.of(context).size.height,
             //   child: ListView.builder(
@@ -248,3 +238,100 @@ class _SellerHomeScreenState extends State<SellerHomeScreen> {
   }
 }
 
+class OrderTile extends StatefulWidget {
+  final OrderModel? order;
+  final String buyerPhone;
+  final String buyerName;
+  final String txnId;
+  final String status;
+  final int totalAmount;
+  OrderTile(
+      {Key? key,
+      this.order,
+      required this.buyerName,
+      required this.buyerPhone,
+      required this.status,
+      required this.totalAmount,
+      required this.txnId})
+      : super(key: key);
+
+  @override
+  _OrderTileState createState() => _OrderTileState();
+}
+
+class _OrderTileState extends State<OrderTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: AppColors.signIn,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.buyerPhone,
+                style: AppTypography.textMd
+                    .copyWith(fontSize: 15, fontWeight: FontWeight.w500),
+              ),
+              //const Spacer(),
+              Text(
+                widget.buyerName,
+                style: AppTypography.textMd
+                    .copyWith(fontSize: 15, fontWeight: FontWeight.w400),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+                    decoration: BoxDecoration(
+                        color: AppColors.backgroundOrange,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Text(
+                      "CONFIRM",
+                      style: AppTypography.textMd.copyWith(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+                    decoration: BoxDecoration(
+                        color: AppColors.backgroundYellow,
+                        border: Border.all(
+                            width: 1, color: AppColors.backgroundOrange),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Text(
+                      "VIEW",
+                      style: AppTypography.textMd.copyWith(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+              //Spacer(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}

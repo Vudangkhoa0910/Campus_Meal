01c002fbf607c5178076ff71174690caf8cd3ba2
namphan06 <namphan06124@gmail.com>
2024-09-27 22:20:05 +0700

    Update /screens/seller_home_sc v5.4

--- lib/screens/seller_home_screen.dart
+++ lib/screens/seller_home_screen.dart
@@ -8,7 +8,7 @@ import 'package:campus_catalogue/services/database_service.dart';
 import 'package:flutter/material.dart';
 
 class OrderWrapper extends StatelessWidget {
-  final List orders;
+  final List<dynamic> orders;
   const OrderWrapper({super.key, required this.orders});
 
   @override
@@ -19,7 +19,6 @@ class OrderWrapper extends StatelessWidget {
         itemCount: orders.length,
         itemBuilder: (context, index) {
           final order = orders[index];
-          print(orders.length);
           return Column(
             children: [
               OrderTile(
@@ -29,9 +28,7 @@ class OrderWrapper extends StatelessWidget {
                 totalAmount: order["total_amount"],
                 txnId: order["txnId"],
               ),
-              SizedBox(
-                height: 10,
-              )
+              const SizedBox(height: 10),
             ],
           );
         },
@@ -42,218 +39,244 @@ class OrderWrapper extends StatelessWidget {
 
 class SellerHomeScreen extends StatefulWidget {
   final ShopModel shop;
-  SellerHomeScreen({Key? key, required this.shop}) : super(key: key);
+
+  const SellerHomeScreen({Key? key, required this.shop}) : super(key: key);
 
   @override
   _SellerHomeScreenState createState() => _SellerHomeScreenState();
 }
 
 class _SellerHomeScreenState extends State<SellerHomeScreen> {
-  List<dynamic> menu = [];
-  DatabaseService service = DatabaseService();
-  Future<List> getOrders() async {
-    List tmp = [];
-    final orders = await FirebaseFirestore.instance
+  List<Map<String, dynamic>> menu = [];
+  final DatabaseService service = DatabaseService();
+
+  Future<List<dynamic>> getOrders() async {
+    final ordersSnapshot = await FirebaseFirestore.instance
         .collection("orders")
         .where("shop_id", isEqualTo: widget.shop.shopID)
         .limit(10)
         .get();
-    for (var doc in orders.docs) {
-      tmp.add(doc);
-    }
-    return tmp;
+
+    return ordersSnapshot.docs.map((doc) => doc.data()).toList();
+  }
+
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  List<Widget> _widgetOptions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _widgetOptions = [
+      homePage(),
+      Text("History"),
+      Text("Notifications"),
+      Text("Profile")
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    for (var item in widget.shop.menu) {
-      Map<String, dynamic> tmp = {
-        "name": item["name"],
-        "price": item["price"],
-        "vegetarian": item["vegetarian"],
-        "description": item["description"],
-        "category": item["category"],
-      };
-
-      menu.add(tmp);
-    }
-
-    int _selectedIndex = 0;
+    menu = List<Map<String, dynamic>>.from(widget.shop.menu.map((item) => {
+          "name": item["name"],
+          "price": item["price"],
+          "vegetarian": item["vegetarian"],
+          "description": item["description"],
+          "category": item["category"],
+        }));
 
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_outlined,
-              color: Colors.black,
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined, color: Colors.black),
+              label: 'Home',
             ),
-            label: 'Home',
-            backgroundColor: Colors.white,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history, color: Colors.black),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications, color: Colors.black),
+              label: 'Notifications',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_outlined, color: Colors.black),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: 0, // _selectedIndex should be managed if needed
+          selectedItemColor: Colors.black,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
+        body: _widgetOptions.elementAt(_selectedIndex));
+  }
+
+  Widget homePage() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 56, 20, 0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Center(
+            child: Text(
+              "Explore IITG",
+              style: AppTypography.textMd.copyWith(
+                fontWeight: FontWeight.w700,
+                color: AppColors.backgroundOrange,
+              ),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history, color: Colors.black),
-            label: 'History',
+          const SizedBox(height: 20),
+          _buildIncomeCard(),
+          const SizedBox(height: 12),
+          Text(
+            "Shop Management",
+            style: AppTypography.textMd.copyWith(
+              fontWeight: FontWeight.w700,
+              fontSize: 20,
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications, color: Colors.black),
-            label: 'Notifications',
+          const SizedBox(height: 32),
+          _buildUpdateMenuButton(context),
+          const SizedBox(height: 12),
+          Text(
+            "Current Orders",
+            style: AppTypography.textMd.copyWith(fontWeight: FontWeight.w700),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_outlined, color: Colors.black),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.black,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 56, 20, 0),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Center(
-              child: Text(
-                "Explore IITG",
-                style: AppTypography.textMd.copyWith(
+          FutureBuilder<List<dynamic>>(
+            future: getOrders(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+              if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                final orders = snapshot.data!;
+                return OrderWrapper(orders: orders);
+              } else {
+                return Text(
+                  "No orders",
+                  style: AppTypography.textMd.copyWith(
+                    fontSize: 20,
                     fontWeight: FontWeight.w700,
-                    color: AppColors.backgroundOrange),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              padding: const EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                  color: AppColors.backgroundYellow,
-                  borderRadius: BorderRadius.circular(10),
-                  border:
-                      Border.all(width: 2, color: AppColors.backgroundOrange)),
-              child: Row(
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        "Today’s income",
-                        style: AppTypography.textMd.copyWith(
-                            fontSize: 14, fontWeight: FontWeight.w700),
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        "Rs. 0.00",
-                        style: AppTypography.textMd.copyWith(
-                            fontWeight: FontWeight.w700,
-                            color: AppColors.backgroundOrange),
-                      )
-                    ],
                   ),
-                  Spacer(),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Upi ID",
-                        style: AppTypography.textMd.copyWith(
-                            fontWeight: FontWeight.w700, fontSize: 14),
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        widget.shop.upiId,
-                        style: AppTypography.textMd.copyWith(fontSize: 12),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            Text(
-              "Shop Management",
-              style: AppTypography.textMd
-                  .copyWith(fontWeight: FontWeight.w700, fontSize: 20),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Center(
-              child: GestureDetector(
-                onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => EditMenu(menu: menu))),
-                child: Container(
-                  width: 340,
-                  padding: const EdgeInsets.fromLTRB(20, 35, 20, 35),
-                  decoration: BoxDecoration(
-                      color: AppColors.signIn,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Center(
-                    child: Text(
-                      "Update Menu",
-                      style: AppTypography.textMd
-                          .copyWith(fontWeight: FontWeight.w700, fontSize: 20),
-                    ),
-                  ),
+                );
+              }
+            },
+          ),
+        ]),
+      ),
+    );
+  }
+
+  Widget _buildIncomeCard() {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: AppColors.backgroundYellow,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(width: 2, color: AppColors.backgroundOrange),
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Today’s income",
+                style: AppTypography.textMd.copyWith(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
+              const SizedBox(height: 4),
+              Text(
+                "Rs. 0.00",
+                style: AppTypography.textMd.copyWith(
+                  fontWeight: FontWeight.w700,
+                  color: AppColors.backgroundOrange,
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "UPI ID",
+                style: AppTypography.textMd.copyWith(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 14,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                widget.shop.upiId,
+                style: AppTypography.textMd.copyWith(fontSize: 12),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUpdateMenuButton(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => EditMenu(menu: menu)),
+        ),
+        child: Container(
+          width: 340,
+          padding: const EdgeInsets.symmetric(vertical: 35),
+          decoration: BoxDecoration(
+            color: AppColors.signIn,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: Text(
+              "Update Menu",
+              style: AppTypography.textMd.copyWith(
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+              ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Current Orders",
-              style: AppTypography.textMd.copyWith(fontWeight: FontWeight.w700),
-            ),
-            FutureBuilder<List<dynamic>>(
-                future: getOrders(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<dynamic>> snapshot) {
-                  if (snapshot.hasData) {
-                    final orders = snapshot.data!;
-                    return OrderWrapper(orders: orders);
-                  } else {
-                    return Text("No orders",
-                        style: AppTypography.textMd.copyWith(
-                            fontSize: 20, fontWeight: FontWeight.w700));
-                  }
-                }),
-          ]),
+          ),
         ),
       ),
     );
   }
 }
 
-class OrderTile extends StatefulWidget {
-  final OrderModel? order;
+class OrderTile extends StatelessWidget {
   final String buyerPhone;
   final String buyerName;
   final String txnId;
   final String status;
   final int totalAmount;
-  OrderTile(
-      {Key? key,
-      this.order,
-      required this.buyerName,
-      required this.buyerPhone,
-      required this.status,
-      required this.totalAmount,
-      required this.txnId})
-      : super(key: key);
 
-  @override
-  _OrderTileState createState() => _OrderTileState();
-}
+  const OrderTile({
+    Key? key,
+    required this.buyerName,
+    required this.buyerPhone,
+    required this.status,
+    required this.totalAmount,
+    required this.txnId,
+  }) : super(key: key);
 
-class _OrderTileState extends State<OrderTile> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -268,83 +291,69 @@ class _OrderTileState extends State<OrderTile> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                widget.buyerPhone,
-                style: AppTypography.textMd
-                    .copyWith(fontSize: 15, fontWeight: FontWeight.w500),
+                buyerPhone,
+                style: AppTypography.textMd.copyWith(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-              //const Spacer(),
               Text(
-                widget.buyerName,
-                style: AppTypography.textMd
-                    .copyWith(fontSize: 15, fontWeight: FontWeight.w400),
-              )
+                buyerName,
+                style: AppTypography.textMd.copyWith(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
-                    decoration: BoxDecoration(
-                        color: AppColors.backgroundOrange,
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Text(
-                      "CONFIRM",
-                      style: AppTypography.textMd.copyWith(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
-                    decoration: BoxDecoration(
-                        color: AppColors.backgroundYellow,
-                        border: Border.all(
-                            width: 1, color: AppColors.backgroundOrange),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Text(
-                      "REJECT",
-                      style: AppTypography.textMd.copyWith(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
-                    decoration: BoxDecoration(
-                        color: AppColors.backgroundYellow,
-                        border: Border.all(
-                            width: 1, color: AppColors.backgroundOrange),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Text(
-                      "VIEW",
-                      style: AppTypography.textMd.copyWith(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black),
-                    ),
-                  ),
+                  _buildActionButton(
+                      "CONFIRM", AppColors.backgroundOrange, Colors.white),
+                  const SizedBox(width: 8),
+                  _buildActionButton(
+                      "REJECT", AppColors.backgroundYellow, Colors.black),
+                  const SizedBox(width: 8),
+                  _buildActionButton(
+                      "VIEW", AppColors.backgroundYellow, Colors.black),
                 ],
               ),
               Text(
-                "Rs." + widget.totalAmount.toString(),
+                "Rs. $totalAmount",
                 textAlign: TextAlign.end,
-                style: AppTypography.textMd
-                    .copyWith(fontWeight: FontWeight.w700, fontSize: 10),
-              )
+                style: AppTypography.textMd.copyWith(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 10,
+                ),
+              ),
             ],
-          )
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildActionButton(
+      String label, Color backgroundColor, Color textColor) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Text(
+        label,
+        style: AppTypography.textMd.copyWith(
+          color: textColor,
+          fontWeight: FontWeight.w400,
+          fontSize: 12,
+        ),
       ),
     );
   }

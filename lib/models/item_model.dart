bfff4e79478865e14af51a634f973305f36d3eb9
namphan06 <namphan06124@gmail.com>
2024-09-30 10:50:02 +0700

    Update item_model v1

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ItemModel {
   String name;
-  String price;
+  num price;
   String category;
   bool vegetarian;
   String description;

0aa46a6b1e1281aef097c122fbef599b829170b9
namphan06 <namphan06124@gmail.com>
2024-09-30 14:47:33 +0700

    Update shop_model v2

--- lib/models/shopModel.dart
+++ lib/models/shopModel.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ShopModel {
   String shopID;
   String upiId;
@@ -11,23 +13,24 @@ class ShopModel {
   bool? status;
   String alternatePhoneNumber;
   List menu;
-  // Map<String, int> rating;
+  Map<String, int> rating;
+  String img;
 
-  ShopModel({
-    required this.shopID,
-    required this.alternatePhoneNumber,
-    required this.closingTime,
-    required this.location,
-    required this.openingTime,
-    required this.ownerName,
-    required this.phoneNumber,
-    required this.shopName,
-    required this.shopType,
-    required this.upiId,
-    required this.menu,
-    this.status,
-    // required this.rating
-  });
+  ShopModel(
+      {required this.shopID,
+      required this.alternatePhoneNumber,
+      required this.closingTime,
+      required this.location,
+      required this.openingTime,
+      required this.ownerName,
+      required this.phoneNumber,
+      required this.shopName,
+      required this.shopType,
+      required this.upiId,
+      required this.menu,
+      this.status,
+      required this.rating,
+      required this.img});
 
   Map<String, dynamic> toMap() {
     return {
@@ -42,8 +45,9 @@ class ShopModel {
       'phone_number': phoneNumber,
       'alternate_phone_number': alternatePhoneNumber,
       'menu': menu,
-      // "rating": rating,
-      "status": status
+      "rating": rating,
+      "status": status,
+      "img": img,
     };
   }
 
@@ -58,7 +62,24 @@ class ShopModel {
         ownerName = sellerMap["owner_name"],
         phoneNumber = sellerMap["phone_number"],
         alternatePhoneNumber = sellerMap["alternate_phone_number"],
-        menu = sellerMap["menu"],
-        // rating = sellerMap["rating"],
-        status = sellerMap["status"];
+        menu = List.from(sellerMap["menu"]),
+        rating = Map<String, int>.from(sellerMap["rating"] ?? {}),
+        status = sellerMap["status"],
+        img = sellerMap["img"];
+
+  ShopModel.fromDocument(DocumentSnapshot doc)
+      : shopID = doc["shop_id"],
+        shopName = doc["shop_name"],
+        upiId = doc["upi_id"],
+        shopType = doc["shop_type"],
+        location = doc["location"],
+        openingTime = doc["opening_time"],
+        closingTime = doc["closing_time"],
+        ownerName = doc["owner_name"],
+        phoneNumber = doc["phone_number"],
+        alternatePhoneNumber = doc["alternate_phone_number"],
+        menu = doc["menu"],
+        rating = doc["rating"],
+        status = doc["status"],
+        img = doc["img"];
 }

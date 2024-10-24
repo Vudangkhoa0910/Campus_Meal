3b921195014d242842cc4062f6abfca0072c7975
namphan06 <namphan06124@gmail.com>
2024-10-24 11:06:48 +0700

    Update buy_model V3

--- lib/models/buy_model.dart
+++ lib/models/buy_model.dart
@@ -4,8 +4,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Buy {
   final String buyerName;
   List<OrderModel> orders;
+  num discount;
 
-  Buy({required this.buyerName, required this.orders});
+  Buy({required this.buyerName, required this.orders, this.discount = 1});
 
   Map<String, dynamic> toMap() {
     // Chuyển đổi danh sách orders thành danh sách map
@@ -19,11 +20,14 @@ class Buy {
 
   Buy.fromMap(Map<String, dynamic> buyMap)
       : buyerName = buyMap["buyer_name"],
-        orders = List<OrderModel>.from(buyMap["orders"].map((orderMap) =>
-            OrderModel.fromMap(orderMap))); // Sử dụng fromMap cho OrderModel
+        orders = List<OrderModel>.from(
+            buyMap["orders"].map((orderMap) => OrderModel.fromMap(orderMap))),
+        discount = buyMap['discount']; // Sử dụng fromMap cho OrderModel
 
   Buy.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc)
       : buyerName = doc.data()!["buyer_name"],
-        orders = List<OrderModel>.from(doc.data()!["orders"].map(
-            (orderMap) => OrderModel.fromMap(orderMap))); // Cũng tương tự ở đây
+        orders = List<OrderModel>.from(doc
+            .data()!["orders"]
+            .map((orderMap) => OrderModel.fromMap(orderMap))),
+        discount = doc.data()!['discount']; // Cũng tương tự ở đây
 }

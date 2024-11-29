f6994a4526441c9da6b18858ad8937d6d71daebe
VuDangKhoa <dangkhoa09102004@gmail.com>
2024-11-29 12:41:27 +0700

    update buy_model.dart v6

--- lib/models/buy_model.dart
+++ lib/models/buy_model.dart
@@ -6,21 +6,25 @@ class Buy {
   List<OrderModel> orders;
   num discount;
   String id;
+  bool pay; // Thêm trường 'pay' vào
 
+  // Cập nhật constructor để bao gồm trường 'pay'
   Buy(
       {required this.buyerName,
       required this.orders,
       this.discount = 1,
-      required this.id});
+      required this.id,
+      this.pay = false}); // Mặc định là false (chưa thanh toán)
 
   Map<String, dynamic> toMap() {
-    // Chuyển đổi danh sách orders thành danh sách map
+    // Chuyển đổi danh sách orders thành danh sách map và thêm trường 'pay'
     return {
       'buyer_name': buyerName,
       'orders': orders
           .map((order) => order.toMap())
           .toList(), // Đảm bảo gọi toMap cho từng order
       'id': id,
+      'pay': pay, // Thêm trường 'pay' vào bản đồ
     };
   }
 
@@ -29,7 +33,8 @@ class Buy {
         orders = List<OrderModel>.from(
             buyMap["orders"].map((orderMap) => OrderModel.fromMap(orderMap))),
         discount = buyMap['discount'],
-        id = buyMap['id']; // Sử dụng fromMap cho OrderModel
+        id = buyMap['id'],
+        pay = buyMap['pay'] ?? false; // Nếu không có trường 'pay', mặc định là false
 
   Buy.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc)
       : buyerName = doc.data()!["buyer_name"],
@@ -37,5 +42,6 @@ class Buy {
             .data()!["orders"]
             .map((orderMap) => OrderModel.fromMap(orderMap))),
         discount = doc.data()!['discount'],
-        id = doc.data()!['id']; // Cũng tương tự ở đây
+        id = doc.data()!['id'],
+        pay = doc.data()!['pay'] ?? false; // Nếu không có trường 'pay', mặc định là false
 }
